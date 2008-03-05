dad60144206bbddec0a5c355a3ba5ba82996f753
tarek <tarek@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-03-05 09:48:14 +0000

    initial commit (ZopeSkel generation)

---
+++ setup.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the tool of plone.recipe.cluster
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '0.1.0'
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' + 
+    read('CHANGES.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('plone', 'recipe', 'cluster', 'README.txt')
+    + '\n' +
+    'Contributors\n' 
+    '************\n'
+    + '\n' +
+    read('CONTRIBUTORS.txt')
+    + '\n' +
+    'Download\n'
+    '********\n'
+    )
+entry_point = 'plone.recipe.cluster:Recipe'
+entry_points = {"zc.buildout": ["default = %s" % entry_point]}
+
+tests_require=['zope.testing']
+
+setup(name='plone.recipe.cluster',
+      version=version,
+      description="ZC Buildout recipe for organizing startup scripts.",
+      long_description=long_description,
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Framework :: Buildout',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: Zope Public License',
+        ],
+      keywords='zope2 buildout',
+      author='Tarek Ziade',
+      author_email='tarek@ziade.org',
+      url='http://svn.plone.org/svn/collective/buildout/plone.recipe.cluster',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.recipe'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'zc.buildout'
+                        # -*- Extra requirements: -*-
+                        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite = 'plone.recipe.cluster.tests.test_docs.test_suite',
+      entry_points=entry_points,
+      )
